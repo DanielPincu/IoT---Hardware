4eae01acbf9f5d47e0e386ce15e7ac8b13681787
DanielPincu <ghepin01@easv365.dk>
2026-04-19 13:04:40 +0200

    updated

--- arduino.ino
+++ arduino.ino
@@ -2,69 +2,55 @@
 #include <OneWire.h>
 #include <DallasTemperature.h>
 
-// ===== WIFI =====
 const char* ssid = "S25";
 const char* password = "TryAgain";
 
-// ===== DS18B20 =====
-#define VCC_PIN 2        // fake VCC 
-#define ONE_WIRE_BUS 3   // DATA pin
-
-OneWire oneWire(ONE_WIRE_BUS);
+OneWire oneWire(3);
 DallasTemperature sensors(&oneWire);
 
-// ===== HTTPS CLIENT =====
 WiFiSSLClient client;
 
 void setup() {
-  Serial.begin(115200);
-  delay(2000);
-
-  Serial.println("BOOT");
-
-  // power sensor 
-  pinMode(VCC_PIN, OUTPUT);
-  digitalWrite(VCC_PIN, HIGH);
+  Serial.begin(9600);
   delay(1000);
+
+  Serial.println("Booting...");
+
+  pinMode(2, OUTPUT);
+  digitalWrite(2, HIGH);
 
   sensors.begin();
 
-  // WiFi connect
-  Serial.print("Connecting WiFi");
+  Serial.print("Connecting to WiFi...");
   WiFi.begin(ssid, password);
 
   while (WiFi.status() != WL_CONNECTED) {
-    delay(500);
-    Serial.print(".");
+    delay(1000);
   }
 
-  Serial.println("\nWiFi OK");
-  Serial.println(WiFi.localIP());
+  Serial.println("Connected!");
 }
 
 void loop() {
-  // read temperature
   sensors.requestTemperatures();
-  float temp = sensors.getTempCByIndex(0);
+  int t = sensors.getTempCByIndex(0);
 
-  Serial.print("Temp: ");
-  Serial.println(temp);
+  Serial.print("Reading temperature... ");
+  Serial.println(t);
 
-  // connect to Render (HTTPS)
   if (client.connect("iot-server-yp8e.onrender.com", 443)) {
-    Serial.println("Connected to server");
 
-    // send GET request
     client.print("GET /temp?value=");
-    client.print(temp);
+    client.print(t);
     client.println(" HTTP/1.1");
     client.println("Host: iot-server-yp8e.onrender.com");
-    client.println("Connection: close");
     client.println();
 
+    Serial.println("Temperature sent");
+
   } else {
-    Serial.println("Connection FAILED");
+    Serial.println("Server FAIL");
   }
 
-  delay(5000);
+  delay(1000);
 }
